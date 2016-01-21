1b8a9ae5d75dcae4bf6aefe4e76cd04380d61c3b
Joey Teng <joey.teng.dev@gmail.com>
2016-01-21 19:28:11 +0800

    __tmpl__.py: Import tmpl module from pardir

---
+++ utilities/__tmpl__.py
@@ -0,0 +1,17 @@
+#--coding:utf-8--
+"""
+Import all things in tmpl
+------------
+List:
+    Platform
+"""
+
+if __name__ == '__main__':
+    raise EnvironmentError("DO NOT DIRECTLY RUN THIS SCRIPT!")
+
+import os
+import sys
+sys.path.append(os.path.pardir)
+from tmpl import *
+sys.path.pop()
+
