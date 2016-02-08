63265a63158d6cfe9ce740492837030cddea9067
Joey Teng <joey.teng.dev@gmail.com>
2016-02-08 22:38:26 +0800

    Hard link $utilities/__tmpl.py to $src/__tmpl__.py for importing files in $tmpl

---
+++ tmpl/__tmpl__.py
@@ -0,0 +1,20 @@
+#--coding:utf-8--
+"""
+Import all things in tmpl
+------------
+List:
+    cfgFile
+    Extractor
+    Platform
+    Prompt
+"""
+
+if __name__ == '__main__':
+    raise EnvironmentError("DO NOT DIRECTLY RUN THIS SCRIPT!")
+
+import os
+import sys
+sys.path.append(os.path.pardir)
+from tmpl import *
+sys.path.pop()
+
